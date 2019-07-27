ab67b10d1acc652346f4dfd2eb13a389b50c0a19
Stanislav Dagaev <460sochi@gmail.com>
2019-07-27 16:20:18 +0300

    Last changes before first release

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"fmt"      // пакет для форматированного ввода вывода
+	"log"      // пакет для логирования
+	"net/http" // пакет для поддержки HTTP протокола
+	// пакет для работы с  UTF-8 строками
 )
 
 func main() {
+	port := flag.String("port", "3000", "an int")
 
-	msg := hello()
+	flag.Parse()
 
-	fmt.Println(msg)
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServe(":"+*port, nil) // задаем слушать порт
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
 func hello() string {
 	return "Hello world"
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, hello())
 }
