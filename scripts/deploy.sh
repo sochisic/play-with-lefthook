80a71d674a99667d33c9bfae8e29374801f830de
Stanislav Dagaev <460sochi@gmail.com>
2019-08-04 22:54:37 +0300

    Improving deploy process

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-ssh -i "aws-bot-vps.pem" -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com
+ssh -i "scripts\aws-bot-vps.pem" -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com
 docker stop play
 docker rmi sochisic/play-with-lefthook
 docker run -d -p 3000:3000 --name play --rm sochisic/play-with-lefthook
