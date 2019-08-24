0fa3e4ea12a65ce3a82fe5b99cb1da54d3692e88
Stanislav Dagaev <460sochi@gmail.com>
2019-08-24 20:49:34 +0300

    Try it

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 eval "$(ssh-agent)"
-# echo | ssh-add "./scripts/aws-bot-vps.pem"
-ssh -tt -i "scripts/aws-bot-vps.pem" -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com << EOF
+# echo \\n | ssh-add "./scripts/aws-bot-vps.pem"
+echo \\n | ssh -tt -i "scripts/aws-bot-vps.pem" -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com << EOF
 docker stop play
 docker rmi sochisic/play-with-lefthook
 docker run -d -p 3000:3000 --name play --restart always sochisic/play-with-lefthook
