cfb82cd406f2057e26d36978ac189f8a1b630726
Stanislav Dagaev <460sochi@gmail.com>
2019-08-24 21:34:30 +0300

    try expect

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,15 @@
 # docker rmi --force sochisic/play-with-lefthook
 # docker run -d -p 3000:3000 --name play --restart always sochisic/play-with-lefthook
 # EOF
-eval "$(ssh-agent)"
-yes '' | ssh -tt -i "scripts/aws-bot-vps.pem" -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com "
-docker stop play &&
-docker rm play &&
-docker rmi --force sochisic/play-with-lefthook &&
-docker run -d -p 3000:3000 --name play --restart always sochisic/play-with-lefthook
-"
+# eval "$(ssh-agent)"
+# echo "" | ssh -tt -i "scripts/aws-bot-vps.pem" -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com "
+# docker stop play &&
+# docker rm play &&
+# docker rmi --force sochisic/play-with-lefthook &&
+# docker run -d -p 3000:3000 --name play --restart always sochisic/play-with-lefthook
+# "
+# eval "$(ssh-agent)"
+spawn ssh -tt -i "scripts/aws-bot-vps.pem" -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com
+expect "Enter passphrase for key 'scripts/aws-bot-vps.pem':"
+send "\n"
+expect EOF
