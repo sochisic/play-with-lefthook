5d5f43090255cebdd8effd014a248e6255e46db2
Stanislav Dagaev <460sochi@gmail.com>
2019-08-24 22:07:18 +0300

    ...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
-# eval "$(ssh-agent)"
-# ssh -tt -i "scripts/aws-bot-vps.pem" -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com << EOF
-# docker stop play
-# docker rm play
-# docker rmi --force sochisic/play-with-lefthook
-# docker run -d -p 3000:3000 --name play --restart always sochisic/play-with-lefthook
-# EOF
+eval "$(ssh-agent)"
+ssh -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com << EOF
+docker stop play
+docker rm play
+docker rmi --force sochisic/play-with-lefthook
+docker run -d -p 3000:3000 --name play --restart always sochisic/play-with-lefthook
+EOF
 # eval "$(ssh-agent)"
 # echo "" | ssh -tt -i "scripts/aws-bot-vps.pem" -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com "
 # docker stop play &&
@@ -13,8 +13,8 @@
 # docker run -d -p 3000:3000 --name play --restart always sochisic/play-with-lefthook
 # "
 # eval "$(ssh-agent)"
-spawn ssh ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com
-expect "Enter passphrase for key 'scripts/aws-bot-vps.pem':"
-send "\n"
-expect "Are you sure you want to continue connecting (yes/no)?:"
-send "yes\n"
+# spawn ssh ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com
+# expect "Enter passphrase for key 'scripts/aws-bot-vps.pem':"
+# send "\n"
+# expect "Are you sure you want to continue connecting (yes/no)?:"
+# send "yes\n"
