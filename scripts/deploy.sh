0ead44d221d1c3c66a7a1542ae02ef53f95dc334
Stanislav Dagaev <460sochi@gmail.com>
2019-09-08 14:11:02 +0300

    Let's try Watchtower for restart my service on ec2 instance

--- scripts/deploy.sh
+++
@@ -1,2 +0,0 @@
-eval "$(ssh-agent)"
-sshpass -p "" ssh -o "StrictHostKeyChecking=no" ubuntu@ec2-13-48-30-65.eu-north-1.compute.amazonaws.com "docker stop play && docker rm play && docker rmi --force sochisic/play-with-lefthook/ && docker run -d -p 3000:3000 --name play --restart always sochisic/play-with-lefthook"
