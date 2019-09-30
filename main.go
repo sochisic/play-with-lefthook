8a25af09233ef875f18047f810d43c1fb2cd3029
Stanislav Dagaev <460sochi@gmail.com>
2019-10-01 02:25:42 +0300

    Some changes for provide HTTPS

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"      // пакет для форматированного ввода вывода
 	"log"      // пакет для логирования
 	"net/http" // пакет для поддержки HTTP протокола
@@ -10,23 +9,19 @@ import (
 )
 
 func main() {
-	port := flag.String("port", "3000", "an int")
-
-	flag.Parse()
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", HelloServer)
 
-	fmt.Printf("Listening port %v...", *port)
+	fmt.Println("Listening port ...")
 	// err := http.ListenAndServe(":"+*port, mux) // задаем слушать порт
-	err := certmagic.HTTPS([]string{"play-with-lefthook.tk"}, mux) // задаем слушать порт
+	err := certmagic.HTTPS([]string{"play-with-lefthook.tk", "www.play-with-lefthook.tk"}, mux) // задаем слушать порт
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
 func hello() string {
-	return "Hello world v0.9.3"
+	return "Hello world v0.9.4"
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
