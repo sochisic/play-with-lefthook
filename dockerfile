0ee34d61b291de6bc7b0dbc265f43509229083ab
Stanislav Dagaev <460sochi@gmail.com>
2019-10-01 02:59:44 +0300

    Looks good, it works!

--- dockerfile
+++ dockerfile
@@ -11,4 +11,6 @@ RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o /bin/play
 FROM golang:1.13-alpine
 WORKDIR /play-with-lefthook
 COPY --from=build /bin/play /bin/play
+EXPOSE 80
+EXPOSE 443
 ENTRYPOINT ["/bin/play"]
--- main.go
+++ main.go
@@ -12,53 +12,14 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", HelloServer)
 
-	// // fmt.Println("Listening port ...")
-
-	// cache := certmagic.NewCache(certmagic.CacheOptions{
-
-	// })
-
-	// magic := certmagic.New(certmagic.Config{
-
-	// 	Email:          "460sochi@gmail.com",
-	// 	Agreed:         true,
-	// 	AltHTTPPort:    80,
-	// 	AltTLSALPNPort: 443,
-	// })
-
-	// err := magic.Manage([]string{"play-with-lefthook.tk", "www.play-with-lefthook.tk"})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// s := http.Server{
-	// 	Addr:      "443",
-	// 	Handler:   mux,
-	// 	TLSConfig: magic.TLSConfig(),
-	// }
-
-	// log.Println("listening...")
-	// go log.Fatal(http.ListenAndServe(":80", magic.HTTPChallengeHandler(mux)))
-	// log.Fatal(s.ListenAndServeTLS("", ""))
-
 	err := certmagic.HTTPS([]string{"play-with-lefthook.tk", "www.play-with-lefthook.tk"}, mux)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-
-	// ln, err := certmagic.Listen([]string{"play-with-lefthook.tk", "www.play-with-lefthook.tk"})
-	// if err != nil {
-	// 	return err
-	// }
-
-	// tlsConfig, err := certmagic.TLS([]string{"play-with-lefthook.tk", "www.play-with-lefthook.tk"})
-	// if err != nil {
-	// 	return err
-	// }
 }
 
 func hello() string {
-	return "Hello world v0.9.4"
+	return "Hello world v0.9.7"
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
