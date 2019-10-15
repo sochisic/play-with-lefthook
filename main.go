19f9af39ec2735f5487f44383e5a1859a04516d7
sdagaev <460sochi@gmail.com>
2019-10-15 23:34:59 +0300

    Turn off redirect, need to find another way

--- main.go
+++ main.go
@@ -12,24 +12,14 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", HelloServer)
 
-	go func() {
-		if err := http.ListenAndServe(":80", http.HandlerFunc(redirectTLS)); err != nil {
-			log.Fatalf("ListenAndServe error: %v", err)
-		}
-	}()
-
-	err := certmagic.HTTPS([]string{"play-with-lefthook.tk", "www.play-with-lefthook.tk"}, mux)
+	err := certmagic.HTTPS([]string{"retard-bot.tk", "www.retard-bot.tk"}, mux)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func redirectTLS(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://"+r.Host+r.RequestURI, http.StatusMovedPermanently)
-}
-
 func hello() string {
-	return "Hello world v0.9.8"
+	return "Hello world v0.9.9"
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
